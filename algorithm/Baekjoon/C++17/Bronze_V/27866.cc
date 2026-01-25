78c86b36ee17fb3216b36007467d4d82d032be03
Han Seokwon <seokbong131@gmail.com>
2026-01-25 23:52:27 +0900

    "문자와 문자열" (Memory: 2024 KB, Time: 0 ms)

---
+++ algorithm/Baekjoon/C++17/Bronze_V/27866.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string str;
+    cin >> str;
+    
+    int i;
+    cin >> i;
+    
+    cout << str[i-1] << "\n";
+    
+    return 0;
+}
