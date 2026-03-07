5f191cf7e8b7875e967e9089c34063817b410d3e
Han Seokwon <seokbong131@gmail.com>
2026-03-07 15:18:24 +0900

    "알고리즘 수업 - 알고리즘의 수행 시간 1" (Memory: 2020 KB, Time: 0 ms)

---
+++ algorithm/Baekjoon/C++17/Bronze_V/24262.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+
+    cout << 1 << '\n';  // floor func. -> return...
+    cout << 0 << '\n';  // O(1)
+
+    return 0;
+}
