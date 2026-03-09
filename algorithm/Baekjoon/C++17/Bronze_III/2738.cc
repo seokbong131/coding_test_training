72d3e755a5e13d33b0a7458fa986ae1b1a9304be
Han Seokwon <seokbong131@gmail.com>
2026-03-10 00:16:07 +0900

    "행렬 덧셈" (Memory: 2152 KB, Time: 0 ms)

--- algorithm/Baekjoon/C++17/Bronze_III/2738.cc
+++ algorithm/Baekjoon/C++17/Bronze_III/2738.cc
@@ -10,9 +10,9 @@ int main() {
     int N, M;
     cin >> N >> M;
     
-    vector<vector<int>> matrix(N, vector<int> (M));
+    vector<vector<int>> matrix(N, vector<int>(M));
     
-    for (int iter = 0; iter < 2; ++iter) {
+    for (int k = 0; k < 2; ++k) {
         for (int i = 0; i < N; ++i) {
             for (int j = 0; j < M; ++j) {
                 int element;
