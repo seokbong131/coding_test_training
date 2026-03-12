938e787345377715638e2b55ff4e842c76199662
Han Seokwon <seokbong131@gmail.com>
2026-03-13 00:02:54 +0900

    "직사각형" (Memory: 2020 KB, Time: 0 ms)

---
+++ algorithm/Baekjoon/C++17/Bronze_V/27323.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    int A, B;
+    cin >> A >> B;
+    
+    cout << A * B << '\n';
+    
+    return 0;
+}
