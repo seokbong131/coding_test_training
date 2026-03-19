0f0ed2193ea4429f495ad21039555cb2affaa05f
Han Seokwon <seokbong131@gmail.com>
2026-03-19 23:47:16 +0900

    "삼각형 외우기" (Memory: 2020 KB, Time: 0 ms)

---
+++ algorithm/Baekjoon/C++17/Bronze_IV/10101.cc
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    int a, b, c;
+    cin >> a >> b >> c;
+    
+    if (a + b + c != 180)
+        cout << "Error" << '\n';
+    else if (a == b && b == c)
+        cout << "Equilateral" << '\n';
+    else if (a == b || b == c || c == a)
+        cout << "Isosceles" << '\n';
+    else cout << "Scalene" << '\n';
+    
+    return 0;
+}
