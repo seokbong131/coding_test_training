4a0682873e3d624938608139ac5845b49042e85c
Han Seokwon <seokbong131@gmail.com>
2026-03-20 02:05:55 +0900

    "수학은 체육과목 입니다" (Memory: 2020 KB, Time: 0 ms)

---
+++ algorithm/Baekjoon/C++17/Bronze_III/15894.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    long long n;
+    cin >> n;
+    
+    cout << 4 * n << '\n';
+    
+    return 0;
+}
