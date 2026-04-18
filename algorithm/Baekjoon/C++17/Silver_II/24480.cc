7c227af9ae463e23a2114e9ea6369d38bed36ca1
Han Seokwon <seokbong131@gmail.com>
2026-04-18 23:01:33 +0900

    "알고리즘 수업 - 깊이 우선 탐색 2" (Memory: 15620 KB, Time: 84 ms)

---
+++ algorithm/Baekjoon/C++17/Silver_II/24480.cc
@@ -0,0 +1,61 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void addEdge(vector<vector<int>>& graph, int u, int v) {
+    graph[u].push_back(v);
+    graph[v].push_back(u);
+}
+
+void dfs(vector<vector<int>>& graph, vector<bool>& isVisted,
+         vector<int>& order, int r, int& cnt) {
+    isVisted[r] = true;
+    
+    cnt++;
+    order[r] = cnt;
+    
+    if (graph[r].empty()) {
+        return;
+    }
+    else {
+        for (int x : graph[r]) {
+            if (isVisted[x] == false) {
+                dfs(graph, isVisted, order, x, cnt);
+            }
+        }
+    }
+}
+
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    
+    int n, m, r;
+    cin >> n >> m >> r;
+
+    vector<vector<int>> graph(n + 1);
+
+    while (m--) {
+        int u, v;
+        cin >> u >> v;
+
+        addEdge(graph, u, v);
+    }
+
+    for (int i = 1; i <= n; ++i) {
+        sort(graph[i].begin(), graph[i].end(), greater<int>());
+    }
+
+    int cnt = 0;
+    vector<bool> isVisited(n + 1);
+    vector<int> order(n + 1);
+
+    dfs(graph, isVisited, order, r, cnt);
+
+    for (int i = 1; i <= n; ++i) {
+        cout << order[i] << '\n';
+    }
+
+    return 0;
+}
