aae93e9759ab2b9723ea520ea019de2efdebd712
Han Seokwon <seokbong131@gmail.com>
2026-04-18 23:03:12 +0900

    "알고리즘 수업 - 너비 우선 탐색 1" (Memory: 9436 KB, Time: 80 ms)

---
+++ algorithm/Baekjoon/C++17/Silver_II/24444.cc
@@ -0,0 +1,68 @@
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+void addEdge(vector<vector<int>>& graph, int u, int v) {
+    graph[u].push_back(v);
+    graph[v].push_back(u);
+}
+
+void bfs(vector<vector<int>>& graph, queue<int>& q,
+         vector<bool>& isVisited, vector<int>& order, int& cnt) {
+    while (q.empty() == false) {
+        int cur = q.front();
+        q.pop();
+
+        for (int x : graph[cur]) {
+            if (isVisited[x] == false) {
+                isVisited[x] = true;
+
+                cnt++;
+                order[x] = cnt;
+
+                q.push(x);
+            }
+        }
+    }
+}
+
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    
+    int n, m, r;
+    cin >> n >> m >> r;
+
+    vector<vector<int>> graph(n + 1);
+
+    while (m--) {
+        int u, v;
+        cin >> u >> v;
+
+        addEdge(graph, u, v);
+    }
+
+    for (int i = 1; i <= n; ++i) {
+        sort(graph[i].begin(), graph[i].end());
+    }
+
+    queue<int> q;
+    q.push(r);
+
+    vector<bool> isVisited(n + 1);
+    isVisited[r] = true;
+
+    int cnt = 1;
+    vector<int> order(n + 1);
+    order[r] = cnt;
+
+    bfs(graph, q, isVisited, order, cnt);
+
+    for (int i = 1; i <= n; ++i) {
+        cout << order[i] << '\n';
+    }
+
+    return 0;
+}
