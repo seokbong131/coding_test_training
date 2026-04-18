89924988e1156cf602c0a9a3064b02cbb1a1dd7e
Han Seokwon <seokbong131@gmail.com>
2026-04-18 22:17:54 +0900

    "수 정렬하기 2" (Memory: 5928 KB, Time: 256 ms)

---
+++ algorithm/Baekjoon/C++17/Silver_V/2751.cc
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+
+    vector<int> num(n);
+    for (auto& x : num) {
+        cin >> x;
+    }
+
+    sort(num.begin(), num.end());
+
+    for (auto& x : num) {
+        cout << x << '\n';
+    }
+
+    return 0;
+}
